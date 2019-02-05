0ac2fa7c542e745ba5316de265e95cb5c38b5ac6
rajiv talluri <tallurirajiv@gmail.com>
2019-02-06 01:29:51 +0530

    add placeholders and use git commit as docker image tag

--- caeAPI/src/main/rcs/docker/docker-entrypoint.sh
+++ caeAPI/src/main/rcs/docker/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle &&
-                         java -jar /app.jar --server.port=80  --management.port=81
+                         java -Xmx@java.xmx.mb@m -Xms@java.xms.mb@m -jar /app.@project.packaging@ --server.port=80  --management.port=81

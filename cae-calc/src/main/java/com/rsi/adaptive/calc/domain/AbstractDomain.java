f9fd0f75ba0e3f61fe1273bbe5996b68573aaf20
prathima suryadevara <prathima.suryadevara@hmhco.com>
2019-02-11 14:59:47 -0600

    re-naming module names

--- cae-calc/src/main/java/com/rsi/adaptive/calc/domain/AbstractDomain.java
+++ cae-calc/src/main/java/com/rsi/adaptive/calc/domain/AbstractDomain.java
@@ -7,7 +7,6 @@ import javax.persistence.MappedSuperclass;
 /**
  * Created by suryadevarap on 1/8/19.
  */
-@MappedSuperclass
 public class AbstractDomain implements Serializable {
 
   private static final long serialVersionUID = -2313566881552390694L;
